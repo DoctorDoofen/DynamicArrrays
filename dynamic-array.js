48650fc3d3494d22c90018f62d8ddd938a6f61b5
DoctorDoofen <and.bennett0930@gmail.com>
2024-06-19 22:39:06 -0500

    all done

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,25 +1,30 @@
 class DynamicArray {
 
-  constructor(data, capacity, length, defaultSize = 4) {
+  constructor(defaultSize = 4) {
     this.data = new Array(defaultSize)
-    this.capacity = 4
+    this.capacity = defaultSize
     this.length = 0
 
   }
 
   read(index) {
 
-    // Your code here 
+    return this.data[index]
   }
 
   unshift(val) {
-
-    // Your code here 
+    for (let i = this.data.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1]
+    }
+    this.data[0] = val
+    this.length++
   }
 
 }
 
-let test = new DynamicArray()
+let test = new DynamicArray(8)
 console.log(test)
-console.log()
+test.unshift(3)
+console.log(test)
+
 module.exports = DynamicArray;
